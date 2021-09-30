bb317913ae075cc162e2ab929283ae8a772ca463
石川恵実里 <emiri@emirinoMacBook-Pro.local>
2021-09-30 21:49:18 +0900

    added redirect in case of accessed to non-existing URL

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,35 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import BookIndex from '@/pages/BookIndex.vue'
-import BookEdit from '@/pages/BookEdit.vue'
-import BookSearch from '@/pages/BookSearch.vue'
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import BookIndex from "@/pages/BookIndex.vue";
+import BookEdit from "@/pages/BookEdit.vue";
+import BookSearch from "@/pages/BookSearch.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
-    name: 'BookIndex',
-    component: BookIndex
+    path: "/",
+    name: "BookIndex",
+    component: BookIndex,
   },
   {
-    path: '/search',
-    name: 'BookSearch',
-    component: BookSearch
+    path: "/search",
+    name: "BookSearch",
+    component: BookSearch,
   },
   {
-    path: '/edit',
-    name: 'BookEdit',
-    component: BookEdit
+    path: "/edit",
+    name: "BookEdit",
+    component: BookEdit,
   },
-
-]
+  {
+    path: "*",
+    redirect: "/",
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
