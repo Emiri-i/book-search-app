0cc4195b678b4e1da81926754156040b415cd1a4
石川恵実里 <emiri@emirinoMacBook-Pro.local>
2021-10-10 16:26:01 +0900

    created method for updateing bookinfo

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
     <Header />
     <v-main>
       <v-container>
-        <router-view @add-book-list="addBook" :books="books" />
+        <router-view
+          @add-book-list="addBook"
+          :books="books"
+          @update-book-info="updateBookInfo"
+        />
       </v-container>
     </v-main>
     <Footer />
@@ -63,6 +67,19 @@ export default Vue.extend({
     saveBooks() {
       const parsed = JSON.stringify(this.books);
       localStorage.setItem(STORAGE_KEY, parsed);
+    },
+    updateBookInfo(e: bookType) {
+      const updateInfo: bookType = {
+        id: e.id,
+        readDate: e.readDate,
+        memo: e.memo,
+        title: this.books[e.id].title,
+        image: this.books[e.id].image,
+        description: this.books[e.id].description,
+      };
+      this.books.splice(e.id, 1, updateInfo);
+      this.saveBooks();
+      this.$router.push("/");
     },
     goToEditPage(id: number) {
       this.$router.push(`/edit/${id}`);
