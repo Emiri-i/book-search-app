857412fcbee41860e77577a9add557881a93a032
石川恵実里 <emiri@emirinoMacBook-Pro.local>
2021-10-10 16:48:43 +0900

    created UI

--- src/pages/BookIndex.vue
+++ src/pages/BookIndex.vue
@@ -1,15 +1,57 @@
 <template>
-  <div>BookIndex</div>
+  <div>
+    <v-row>
+      <v-col cold="6">
+        <v-btn color="primary" to="/search">search books</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="6" v-for="book in books" :key="book.id">
+        <v-card>
+          <v-row>
+            <v-col cols="4">
+              <v-img :src="book.image"></v-img>
+            </v-col>
+            <v-col cols="12">
+              <v-card-title>
+                {{ book.title }}
+                読んだ日：{{ book.readDate }} 感想：{{ book.readDate }}
+                <v-spacer></v-spacer>
+                <v-card-actions>
+                  <v-btn
+                    :to="{ name: 'BookEdit', params: { id: book.id } }"
+                    color="indigo"
+                    fab
+                    small
+                    dark
+                  >
+                    <v-icon>
+                      mdi-pencil
+                    </v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-card-title>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue, { PropType } from "vue";
+import { bookType } from "@/libs/types";
 export default Vue.extend({
   name: "BookIndex",
   data() {
-    return {
-      value: "hoge",
-    };
+    return {};
+  },
+  props: {
+    books: {
+      type: Array as PropType<bookType[]>,
+      default: () => [],
+    },
   },
 });
 </script>
