c9d81183ccef003b7ce085fe7ebb04f3f5864bd1
石川恵実里 <emiri@emirinoMacBook-Pro.local>
2021-10-11 23:08:30 +0900

    added nav-bar title

--- src/global/Header.vue
+++ src/global/Header.vue
@@ -3,13 +3,32 @@
     <v-app-bar app color="primary" dark>
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Find a book</v-toolbar-title>
+      <v-toolbar-title>
+        Find a book
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn color="error" @click="deleteLocalStorage">
         Delete all
       </v-btn>
     </v-app-bar>
     <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list-item>
+        <v-list-item-content>
+          <!-- <div class="d-flex align-center justify-center"> -->
+          <v-list-item-title class="text-h6">
+            <v-icon left>
+              mdi-book-open-variant
+            </v-icon>
+            Book Seach App
+          </v-list-item-title>
+          <!-- </div> -->
+          <v-list-item-subtitle>
+            broden your knowledge
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
       <v-list nav dense>
         <v-list-item-group v-model="selectedItem" active-class="blue lighten-4">
           <v-list-item v-for="(item, i) in items" :key="i" :to="item.url">
