3c99dde4a415483b77b6702d90bb6d2c6a35571f
石川恵実里 <emiri@emirinoMacBook-Pro.local>
2021-10-19 23:38:05 +0900

    fixed ui

--- src/pages/BookSearch.vue
+++ src/pages/BookSearch.vue
@@ -1,35 +1,27 @@
 <template>
   <div>
     <v-row>
-      <v-col class="text-h6">
-        Search Books
-        <v-icon>
+      <v-col class="text-h6 d-flex align-center">
+        <v-icon class="mr-2">
           mdi-book-search
         </v-icon>
+        Search Books
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="6">
+      <v-col cols="6" class="d-flex align-center">
         <v-text-field
           label="Search by book title"
           v-model="keyword"
           filled
           dense
+          hide-details="auto"
         ></v-text-field>
-      </v-col>
-    </v-row>
-    <!-- <v-row>
-      <v-col cols="3">
-        <v-btn color="primary" @click="search(keyword)">
+        <v-btn color="primary" @click="search(keyword)" class="ml-10">
           Search
         </v-btn>
       </v-col>
-      <v-col cols="3">
-        <v-btn color="secondary" to="/">
-          Back to Index
-        </v-btn>
-      </v-col>
-    </v-row> -->
+    </v-row>
     <v-row>
       <v-col
         cols="12"
@@ -37,26 +29,28 @@
         v-for="(book, index) in searchResults"
         :key="book.index"
       >
-        <v-card>
-          <v-row>
-            <v-col cols="4">
-              <v-img :src="book.img"></v-img>
-            </v-col>
-            <v-col cols="8">
-              <v-card-title>
-                {{ book.title }}
-              </v-card-title>
-              {{ book.description }}
-              <v-spacer></v-spacer>
-              <v-card-actions>
-                <v-btn fab dark color="indigo" @click="addBookList(index)">
-                  <v-icon dark>
-                    mdi-plus
-                  </v-icon>
-                </v-btn>
-              </v-card-actions>
-            </v-col>
-          </v-row>
+        <v-card style="height:250px;">
+          <v-container>
+            <v-row>
+              <v-col cols="4" class="d-flex align-center" style="height:230px;">
+                <v-img :src="book.img"></v-img>
+              </v-col>
+              <v-col cols="8">
+                <v-card-title>
+                  {{ book.title }}
+                </v-card-title>
+                {{ book.description }}
+                <v-spacer></v-spacer>
+                <v-card-actions>
+                  <v-btn fab dark color="indigo" @click="addBookList(index)">
+                    <v-icon dark>
+                      mdi-plus
+                    </v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-col>
+            </v-row>
+          </v-container>
         </v-card>
       </v-col>
     </v-row>
