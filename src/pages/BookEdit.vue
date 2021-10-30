26ab59c395cfc4b00f89a6804f4061e3a8f2bfa9
石川恵実里 <emiri@emirinoMacBook-Pro.local>
2021-10-30 18:42:36 +0900

    changed japanese letters to English

--- src/pages/BookEdit.vue
+++ src/pages/BookEdit.vue
@@ -3,45 +3,56 @@
     <v-row>
       <v-col cols="12">
         <v-card class="mx-auto">
-          <v-row>
-            <v-col cols="4">
-              <v-img :src="book.image"></v-img>
-            </v-col>
-            <v-col cols="8">
-              <v-card-title> タイトル：{{ book.title }} </v-card-title>
-              読んだ日：
-              <v-menu
-                v-model="menu"
-                :close-on-content-click="false"
-                :nudge-right="40"
-                transition="scale-transition"
-                offset-y
-                min-width="auto"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
+          <v-container>
+            <v-row>
+              <v-col cols="4">
+                <v-img :src="book.image"></v-img>
+              </v-col>
+              <v-col cols="8">
+                <v-card-title class="font-weight-bold ">
+                  {{ book.title }}
+                </v-card-title>
+                <!-- finished date -->
+                <v-menu
+                  v-model="menu"
+                  :close-on-content-click="false"
+                  :nudge-right="40"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="auto"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="date"
+                      label="finished date"
+                      placeholder="select date"
+                      prepend-icon="mdi-calendar"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
                     v-model="date"
-                    prepend-icon="mdi-calendar"
-                    readonly
-                    v-bind="attrs"
-                    v-on="on"
-                  ></v-text-field>
-                </template>
-                <v-date-picker
-                  v-model="date"
-                  @input="menu = false"
-                ></v-date-picker>
-              </v-menu>
-              感想：
-              <v-textarea filled class="mx-2" v-model="book.memo">
-                {{ book.memo }}
-              </v-textarea>
-              <v-card-actions>
-                <v-btn color="secondary" to="/">一覧に戻る</v-btn>
-                <v-btn color="info" @click="updateBookInfo">保存する</v-btn>
-              </v-card-actions>
-            </v-col>
-          </v-row>
+                    @input="menu = false"
+                  ></v-date-picker>
+                </v-menu>
+                <v-textarea
+                  v-model="book.memo"
+                  label="comment"
+                  filled
+                  class="mx-2"
+                  name="comment"
+                >
+                  {{ book.memo }}
+                </v-textarea>
+                <v-card-actions>
+                  <!-- <v-btn color="secondary" to="/">go to My Books Page</v-btn> -->
+                  <v-btn color="primary" @click="updateBookInfo">save</v-btn>
+                </v-card-actions>
+              </v-col>
+            </v-row>
+          </v-container>
         </v-card>
       </v-col>
     </v-row>
